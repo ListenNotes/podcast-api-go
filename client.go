802381ed5553ccaaf61926fc17ef6b55b794c72c
Matt Holcomb <yorktownmatt@gmail.com>
2021-05-06 23:37:37 -0400

    added a few more client methods and int tests, supporting GET/{id}, POST and DELETE

--- client.go
+++ client.go
@@ -3,12 +3,18 @@
 package listennotes
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 // HTTPClient is the client interface
 type HTTPClient interface {
 	Search(args map[string]string) (*Response, error)
+	FetchPodcastByID(id string, args map[string]string) (*Response, error)
+	BatchFetchPodcastByID(ids []string, args map[string]string) (*Response, error)
+	DeletePodcast(id string, args map[string]string) (*Response, error)
 }
 
 type standardHTTPClient struct {
@@ -42,5 +48,54 @@ func NewClient(apiKey string, opts ...ClientOption) HTTPClient {
 }
 
 func (c *standardHTTPClient) Search(args map[string]string) (*Response, error) {
-	return c.get(args, "search")
+	return c.get("search", args)
+}
+
+// GET /typeahead
+
+// GET /best_podcasts
+
+func (c *standardHTTPClient) FetchPodcastByID(id string, args map[string]string) (*Response, error) {
+	return c.get(fmt.Sprintf("podcasts/%s", id), args)
+}
+
+// GET /episodes/{id}
+
+// POST /episodes
+func (c *standardHTTPClient) BatchFetchPodcastByID(ids []string, args map[string]string) (*Response, error) {
+	return c.post("episodes", args, url.Values{
+		"ids": []string{strings.Join(ids, ",")},
+	})
+}
+
+// POST /podcasts
+
+// GET /curated_podcasts/{id}
+
+// GET /genres
+
+// GET /regions
+
+// GET /languages
+
+// GET /just_listen
+
+// GET /curated_podcasts
+
+// GET /podcats/{id}/recommendations
+
+// GET /episodes/{id}/recommendations
+
+// GET /playlists
+
+// GET /playlists/{id}
+
+// POST /podcasts/submit
+
+// -H 'Content-Type: application/x-www-form-urlencoded'\
+// -d 'rss=https://feeds.megaphone.fm/committed'\
+// -d 'email=hello@example.com'
+
+func (c *standardHTTPClient) DeletePodcast(id string, args map[string]string) (*Response, error) {
+	return c.delete(fmt.Sprintf("podcasts/%s", id), args)
 }
--- integration_test.go
+++ integration_test.go
@@ -20,62 +20,6 @@ func TestAuthErrorIntegration(t *testing.T) {
 	}
 }
 
-func TestSearchIntegration(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping integration test")
-	}
-
-	rt := &integrationTestRoundTripper{}
-	httpClient := &http.Client{
-		Transport: rt,
-	}
-
-	client := listennotes.NewClient("", listennotes.WithHTTPClient(httpClient))
-
-	expectations := []integrationExpectation{
-		{
-			Method: "GET",
-			Path:   "search",
-			Args: map[string]string{
-				"q": "term",
-			},
-			ExecuteFunc: client.Search,
-			ValidateFunc: func(resp *listennotes.Response, respErr error) error {
-				expectData(resp, "took", 0.234)
-				expectResults(resp, 10)
-				expectNoError(respErr)
-				return nil
-			},
-		},
-	}
-
-	for idx, exp := range expectations {
-		resp, err := exp.ExecuteFunc(exp.Args)
-		func() {
-			defer func() {
-				if r := recover(); r != nil {
-					t.Errorf("[%d] %s failed expectation: %v", idx, exp.Path, r)
-				}
-			}()
-			if err := exp.ValidateFunc(resp, err); err != nil {
-				t.Errorf("[%d] %s validation failed: %s", idx, exp.Path, err)
-			}
-			expectedPath := fmt.Sprintf("/api/v2/%s", exp.Path)
-			if rt.LastRequest.URL.Path != expectedPath {
-				t.Errorf("[%d] %s had unexpected Path: %s", idx, exp.Path, rt.LastRequest.URL.Path)
-			}
-			if rt.LastRequest.Method != exp.Method {
-				t.Errorf("[%d] %s had unexpected Method: %s", idx, exp.Path, rt.LastRequest.Method)
-			}
-			for qk, qv := range exp.Args {
-				if rt.LastRequest.URL.Query().Get(qk) != qv {
-					t.Errorf("[%d] %s did not have expected query arg: %s=%s in %s", idx, exp.Path, qk, qv, rt.LastRequest.URL.RawQuery)
-				}
-			}
-		}()
-	}
-}
-
 type integrationExpectation struct {
 	Method       string
 	Path         string
@@ -84,12 +28,12 @@ type integrationExpectation struct {
 	ValidateFunc func(resp *listennotes.Response, respErr error) error
 }
 
-func expectResults(resp *listennotes.Response, expectedCount int) {
+func expectCollection(resp *listennotes.Response, collectionKey string, expectedCount int) {
 	if resp == nil {
 		panic(fmt.Errorf("no response"))
 	}
 
-	results, ok := resp.Data["results"].([]interface{})
+	results, ok := resp.Data[collectionKey].([]interface{})
 	if !ok {
 		panic(fmt.Errorf("no results"))
 	}
@@ -100,7 +44,7 @@ func expectResults(resp *listennotes.Response, expectedCount int) {
 }
 
 func expectData(resp *listennotes.Response, key string, expectedValue interface{}) {
-	actual, ok := resp.Data["took"]
+	actual, ok := resp.Data[key]
 	if !ok {
 		panic(fmt.Errorf("no data at %s", key))
 	}
